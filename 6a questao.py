887d63db18147138c85e6b0ebb002e90c9360e29
Kvyotho <159950199+Kvyotho@users.noreply.github.com>
2024-10-21 08:47:53 -0300

    21102024

---
+++ 6a questao.py
@@ -0,0 +1,16 @@
+#A)
+def soma_diagonal_principal(matriz):
+    soma = 0
+    for i in range(len(matriz)):
+        soma += matriz[i][i]
+    return soma
+
+#B)
+def soma_diagonal_principal(matriz):
+
+#C)
+def soma_diagonal_principal(matriz):
+#D)
+def soma_diagonal_principal(matriz):
+#E)
+def soma_diagonal_principal(matriz):
